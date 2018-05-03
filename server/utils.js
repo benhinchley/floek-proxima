cbb634de371184a6af5e7bed726cf01dd023e67c
Benjamin Hinchley <benjaminhinchley@gmail.com>
2018-05-03 20:08:53 +1000

    update server/utils

--- server/utils.js
+++ server/utils.js
@@ -1,4 +1,6 @@
 const enforceHTTPS = (req, res, next) => {
+  if (req.get("X-Forwarded-Proto") === undefined) return next();
+
   if (req.get("X-Forwarded-Proto").indexOf("https") != -1) {
     return next();
   } else {
